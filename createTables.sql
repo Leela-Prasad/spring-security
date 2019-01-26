eb62574706e08f8f569597caed4b52574af8c89c
Leela <leela.jagu@gmail.com>
2019-01-26 11:08:31 +0530

    jdbc authentication using custom tables(tbl_users, tbl_role)

--- createTables.sql
+++ createTables.sql
@@ -21,7 +21,22 @@ select * from authorities;
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home
 export DERBY_INSTALL=/Users/Leela/Desktop/spring-workspace/db-derby-10.11.1.1-bin
-export CLASSPATH=$CLASSPATH:$DERBY_INSTALL/lib/derby.jar:$DERBY_INSTALL/lib/derbytools.jar:.
+export CLASSPATH=$CLASSPATH:$DERBY_INSTALL/lib/derby.jar:$DERBY_INSTALL/lib/derbytools.jar:$DERBY_INSTALL/lib/derbyclient.jar
 
 java org.apache.derby.tools.ij
 connect 'jdbc:derby:MyDB;create=true';
+connect 'jdbc:derby://localhost/MyDB2';
+
+
+
+drop table authorities;
+drop table users;
+
+create table tbl_users (username varchar(50) not null primary key, password varchar(150) not null);
+create table tbl_roles(username varchar(50) not null, role varchar(50) not null);
+
+insert into tbl_users values('prasad','secret');
+insert into tbl_users values('xyz', 'password');
+insert into tbl_roles values('prasad', 'ROLE_ADMIN');
+insert into tbl_roles values('prasad', 'ROLE_USER');
+insert into tbl_roles values('xyz', 'ROLE_USER');
